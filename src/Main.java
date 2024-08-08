b8c5f41063c3644c98cae2b314a706aff632b9aa
mohit kumar singh <mertiyakishoresingh@gmail.com>
2024-08-08 22:26:40 +0530

    Add main file in src folder

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,7 @@ public class Main {
         String url="jdbc:mysql://localhost:3306/mydatabase";
         String username="root";
         String password="root";
-        String query = "UPDATE employees " +
-                "SET job_title = 'Game Developer', salary = 90000.0 " +
-                "WHERE id = 2;";
+        String query = "Select * from employees where name = ?  ;";
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -19,19 +17,23 @@ public class Main {
         try {
             Connection con= DriverManager.getConnection(url,username,password);
             System.out.println("connection is setup");
-            Statement stmt = con.createStatement();
-
-            int rowsAffected = stmt.executeUpdate(query);
-
-            if(rowsAffected>0){
-                System.out.println("Updation  Successful: "+rowsAffected);
-            }else {
-                System.out.println("Updation Failed!!" );
+            PreparedStatement preparedStatement =con.prepareStatement(query);
+            preparedStatement.setString(1,"Hemant");
+//            preparedStatement.setString(2,"Game Developer ");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                // java local variable = database variable
+                int id = resultSet.getInt("id");
+                String name=resultSet.getString("name");
+                String job_title=resultSet.getString("job_title");
+                double salary = resultSet.getDouble("salary");
+                System.out.println("ID: "+id);
+                System.out.println("NAME: "+name);
+                System.out.println("JOB TITLE: "+job_title);
+                System.out.println("SALARY: "+salary);
             }
-
-
-
-            stmt.close();
+            resultSet.close();
+            preparedStatement.close();
             con.close();
             System.out.println("conneciton closed successfully");
 
